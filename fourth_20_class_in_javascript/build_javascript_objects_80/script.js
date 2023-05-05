9e4c7c8b18d43276e38d768d97a6e8dd96f175e5
Clevison123 <cdefensor467@gmail.com>
2023-05-05 12:33:27 -0300

    fourth_20_class_in_javascript

---
+++ fourth_20_class_in_javascript/build_javascript_objects_80/script.js
@@ -0,0 +1,18 @@
+//Example 
+var ourDog = {
+    "Name": "Camper",
+    "Legs": 4,
+    "Tails": 1,
+    "Friends": ["Everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+    "Name": "Rex",
+    "Legs": 4,
+    "Tails": 1,
+    "Friends": ["Just Me"]
+}
+
+console.log(myDog);
