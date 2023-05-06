3c2d6aa410c90ee4025ccd351d89dd698dc47f47
Clevison123 <cdefensor467@gmail.com>
2023-05-06 07:35:52 -0300

    fifth_20_lessons_in_javascript

---
+++ fifth_20_lessons_in_javascript/iterate_for_loops_94/script.js
@@ -0,0 +1,17 @@
+// Example 
+var ourArray = [];
+
+for ( var i = 0; i < 5; i++) {
+    ourArray.push(i);
+}
+
+//setup
+var myArray = []
+
+// Only change code below this line.
+
+for (var i = 1; i < 10; i++) {
+    myArray.push(i);
+}
+
+console.log(myArray);
